3d42c354f1efabd38258f5d55d5ac17d2a06243f
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-01-11 18:32:40 +0200

    Finished image splitting

--- tree.py
+++ tree.py
@@ -1,15 +1,125 @@
 import numpy as np
+from dataclasses import dataclass
+import matplotlib.image as img
+import math
+from copy import deepcopy
+
+global_idx = 0
+global_tree = {}
+nodes = []
 
 
 class Node:
-    def __init__(self, tolerance: float, image: np.ndarray):
-        self.tolerance = tolerance
-        self.image = image
+    def __init__(self, idx, w, h, x, y, parent_idx):
+        self.idx = idx
+        self.w = w
+        self.h = h
+        self.x = x
+        self.y = y
+        self.parent_idx = parent_idx
 
-        # every node has 4 descendents as splitting the image into 4 different regions
-        # therefore we have top [ right, left ] and bottom [ right, left ]
 
-        self.top_left = None
-        self.top_right = None
-        self.bottom_left = None
-        self.bottom_right = None
+# criterion
+def pixel_distance_rgb(r: float, g: float, b: float):
+    return math.sqrt(int(r) + int(g) + int(b))
+
+
+def homogenous(idx: int, tolerance: float, image: np.ndarray) -> bool:
+    global nodes
+    node = nodes[idx]
+
+    w, h = node.w, node.h
+
+    if w == 0 or h == 0:
+        return True
+    else:
+        min_distance = 256
+        max_distance = -1
+
+        # Set the ranges for each traversal
+        from_x = node.x
+        from_y = node.y
+        to_x = node.x + w
+        to_y = node.y + h
+        print('From {} to {}'.format(from_x, to_x))
+
+        for r in range(from_x, to_x):
+            for c in range(from_y, to_y):
+
+                r, g, b = image[r][c]
+                d = pixel_distance_rgb(r, g, b)
+
+                if d < min_distance:
+                    min_distance = d
+                if d > max_distance:
+                    max_distance = d
+
+        return (max_distance - min_distance) < tolerance
+
+
+def add_node(parent_idx: int, x: int, y: int, w: int, h: int, tolerance: float, image: np.ndarray) -> Node:
+    # Create a new node as the successor of the current one
+    global global_idx
+    global global_tree
+    global nodes
+
+    new_node = Node(global_idx, w, h, x, y, parent_idx)
+
+    nodes[new_node.idx] = new_node
+    global_tree[new_node.idx] = []
+    global_idx += 1
+
+    if not homogenous(new_node.idx, tolerance, image):
+        split(new_node.idx, tolerance, image)
+    else:
+        print('Added')
+        return new_node
+
+
+def split(idx: int, tolerance: float, image: np.ndarray):
+    global global_tree
+    global nodes
+
+    current_node = nodes[idx]
+    w, h = current_node.w, current_node.h
+    _w, _h = w // 2, h // 2  # new values
+    _wo, _ho = w % 2, h % 2  # offsets
+
+    original_x, original_y = current_node.x, current_node.y
+
+    # Split the image to all successors
+    print('Adding new nodes....')
+    global_tree[idx].append(add_node(idx, original_x, original_y, _w, _h, tolerance, image))
+    global_tree[idx].append(add_node(idx, original_x + _w, original_y, _w + _wo, _h, tolerance, image))
+    global_tree[idx].append(add_node(idx, original_x, original_y + _h, _w, _h + _ho, tolerance, image))
+    global_tree[idx].append(add_node(idx, original_x + _w, original_y + _h, _w + _wo, _h + _ho, tolerance, image))
+
+
+def create_tree(tolerance: float, image: np.ndarray):
+    global global_idx
+    global global_tree
+    global nodes
+
+    w, h, c = image.shape
+    nodes = [None] * (h * w // 2)  # worst case pixel by pixel
+    global_tree = {}
+    global_idx = 0
+
+    root = Node(global_idx, w, h, 0, 0, 0)
+    nodes[global_idx] = root
+    global_idx += 1
+
+    global_tree[root.idx] = []
+    split(root.idx, tolerance, image)
+
+
+if __name__ == '__main__':
+    # test
+    t = 19
+    i = img.imread('images/room.jpeg')
+    create_tree(t, i)
+
+    nodes = list(filter(lambda n: n is not None, nodes))
+    print(nodes)
+    print(global_tree)
+
