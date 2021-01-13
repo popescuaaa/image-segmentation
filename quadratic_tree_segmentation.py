e7b791b93e0da65f038b0477cb638d7d35a16bcb
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-01-13 19:10:10 +0200

    Finished first version of clustering

--- quadratic_tree_segmentation.py
+++ quadratic_tree_segmentation.py
@@ -1,14 +1,24 @@
 import numpy as np
 import matplotlib.image as img
-from metrics import pixel_distance_rgb
+import matplotlib.colors as colors
+from metrics import pixel_distance_rgb, node_cluster_distance
 from copy import deepcopy
 import yaml
+
+# Init environment
 
 global_idx = 0
 global_tree = {}
 nodes = []
+
 out_image = None
 
+tree_list = []
+clusters = []
+cluster_idx = 0
+
+
+# Node class
 
 class Node:
     def __init__(self, idx, w, h, x, y, parent_idx):
@@ -23,6 +33,48 @@ class Node:
         self.mr = 0.0
         self.mg = 0.0
         self.mb = 0.0
+
+        self.cluster_idx = None
+
+
+# Cluster class
+# Clusters encapsulate a list of nodes or regions with similar light intensities
+
+class Cluster:
+    def __init__(self, idx: int):
+        self.idx = idx
+
+        # Mean value for RGB
+        self.mr = 0.0
+        self.mg = 0.0
+        self.mb = 0.0
+
+        # Cluster elements
+        self.elements = []
+
+    def add_node(self, idx: int):
+        global nodes
+        self.elements.append(idx)
+
+        # Recompute mean values
+        mr = 0.0
+        mg = 0.0
+        mb = 0.0
+        for node_idx in self.elements:
+            current_node = nodes[node_idx]
+            mr += current_node.mr
+            mg += current_node.mg
+            mb += current_node.mb
+
+        self.mr = mr / len(self.elements)
+        self.mg = mg / len(self.elements)
+        self.mb = mb / len(self.elements)
+
+    def can_be_added(self, idx: int, tolerance: float):
+        global nodes
+        current_node = nodes[idx]
+        d = node_cluster_distance(self.mr, self.mg, self.mb, current_node.mr, current_node.mg, current_node.mb)
+        return d < tolerance
 
 
 def homogenous(idx: int, tolerance: float, image: np.ndarray) -> bool:
@@ -129,14 +181,16 @@ def create_tree(tolerance: float, image: np.ndarray) -> None:
     split(root.idx, tolerance, image)
 
 
-def apply_mean_rgb_node(idx: int) -> None:
+def apply_mean_rgb_node(idx: int, mr: float or None, mg: float or None, mb: float or None) -> None:
     global global_tree
     global nodes
     global out_image
 
     if len(global_tree[idx]) == 0:
         # Color current node
-        mr, mg, mb = nodes[idx].mr, nodes[idx].mg, nodes[idx].mb
+        if mr is None and mb is None and mg is None:
+            mr, mg, mb = nodes[idx].mr, nodes[idx].mg, nodes[idx].mb
+
         x, y = nodes[idx].x, nodes[idx].y
         w, h = nodes[idx].w, nodes[idx].h
 
@@ -161,6 +215,57 @@ def apply_mean_rgb_node(idx: int) -> None:
         apply_mean_rgb_node(bottom_right_idx)
 
 
+def apply_mean_cluster(idx: int) -> None:
+    global clusters
+
+    for e in clusters[idx].elements:
+        apply_mean_rgb_node(e, clusters[idx].mr, clusters[idx].mg, clusters[idx].mb)
+
+
+def tree_to_list(idx: int):
+    global tree_list
+    if not global_tree[idx]:
+        tree_list.append(idx)
+    else:
+        tree_to_list(global_tree[idx][0])
+        tree_to_list(global_tree[idx][1])
+        tree_to_list(global_tree[idx][2])
+        tree_to_list(global_tree[idx][3])
+
+
+def create_clusters(tolerance: float):
+    global clusters
+    global cluster_idx
+    global tree_list
+
+    base_cluster = Cluster(cluster_idx)
+    cluster_idx += 1
+
+    base_cluster.add_node(tree_list[0])
+    nodes[tree_list[0]].cluster_idx = base_cluster.idx
+
+    clusters.append(base_cluster)
+
+    for node_idx in tree_list[1:]:
+        added = False
+
+        if nodes[node_idx].cluster_idx is None:
+            # It can be added to a cluster
+            for cluster in clusters:
+                if cluster.can_be_added(node_idx, tolerance):
+                    cluster.add_node(node_idx)
+                    nodes[node_idx].cluster_idx = cluster.idx
+                    added = True
+
+            if not added:
+                new_cluster = Cluster(cluster_idx)
+                cluster_idx += 1
+
+                # Add the node to the new cluster
+                new_cluster.add_node(node_idx)
+                clusters.append(new_cluster)
+
+
 if __name__ == '__main__':
 
     # System configuration
@@ -170,11 +275,30 @@ if __name__ == '__main__':
     t = float(config['tolerance'])
     i = img.imread('images/{}'.format(config['target']))
 
+    mode = config['mode']
+
+    # Split
+
+    # Create a quadratic tree
     create_tree(t, i)
+
+    # Convert tree to list
+    tree_to_list(0)
 
     out_image = deepcopy(i)
 
-    for e in global_tree:
-        apply_mean_rgb_node(e)
+    for e in tree_list:
+        apply_mean_rgb_node(e, None, None, None)
 
-    img.imsave('images/{}'.format(config['result']), out_image)
+    img.imsave('images/{}'.format(config['split_result']), out_image)
+
+    # Merge
+
+    out_image = deepcopy(i)
+
+    create_clusters(t)
+
+    for c in clusters:
+        apply_mean_cluster(c.idx)
+
+    img.imsave('images/{}'.format(config['merge_result']), out_image)
