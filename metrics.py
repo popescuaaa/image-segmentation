c7ddab6ef926ec079ebd10c7ccc89f4891bd6a2d
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2021-01-13 19:10:53 +0200

    Added cluster node distance for cluster creation

--- metrics.py
+++ metrics.py
@@ -3,3 +3,7 @@ import math
 
 def pixel_distance_rgb(r: float, g: float, b: float):
     return math.sqrt(int(r) + int(g) + int(b))
+
+
+def node_cluster_distance(c_mr: float, c_mg: float, c_mb: float, n_mr: float, n_mg: float, n_mb: float):
+    return math.sqrt((c_mr - n_mr) ** 2 + (c_mg - n_mg) ** 2 + (c_mb - n_mb) ** 2)
